035da1c1d88c3428a6456bea0b6ce040f5d33d8b
hajarmas <142684280+hajarmas@users.noreply.github.com>
2023-09-12 12:40:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 # Change the following print statement so that it uses the addition operator + and prints out 8
-
+print (4+4)
 # Write a print statement that uses the division operator / 
-
+name = "hajar" 
+age = 17
 # Add a print statement that uses multiplication *
-
+print(f"my name is {name} I'm {age} years old")
 # Create a variable called `length` and assign a value to it
-
+length = 10
 # Create a variable called    `width` and assign a value to it
-
+width = 15
 # Create a variable called `area` and assign to it the multiplication of `length` and `width`
+area = (length*width)
 
 # Print `area` in the following: "The result is " followed by the `area` variable
-
-
+print(f"linth*width")
 # BONUS: Print the area in the following syntax: "If the length of a rectangle is 5, and width is 3 then the area is: 15". Keep in mind that you should replace the numbers with their respective variables. 
+12
